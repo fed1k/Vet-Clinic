b14f970ce0b938b48b8cf72a1626781a5f546d6d
fed1k <allamurotovcoder@mail.ru>
2022-05-31 05:29:09 +0500

    write queries

--- queries.sql
+++ queries.sql
@@ -1,4 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * from animals WHERE name LIKE '%mon%';
-SELECT * from animals WHERE date_of_birth BETWEEN '2016' AND '2019';
+SELECT name from animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
+SELECT name from animals WHERE neutered = true and escape_attempts < 3;
+SELECT date_of_birth from animals where name LIKE 'Agumon' or name LIKE 'Pikachu';
+SELECT name, escape_attempts from animals where weight_kg > 10.5;
+SELECT * from animals where neutered = true;
+SELECT * from animals where name != 'Gabumon';
+SELECT * from animals where weight_kg >= 10.4 and weight_kg <= 17.3;
